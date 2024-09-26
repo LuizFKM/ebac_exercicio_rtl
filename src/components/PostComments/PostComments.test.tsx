cd0a48da4f278dd5a4dd366cc3a1f566776b4ed3
Luiz Francisco <luizfk.gmail.com>
2024-09-25 23:15:13 -0300

    :test_tube: test: Novo teste no componente PostComments

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -4,7 +4,29 @@ import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve renderizar dois comentarios', () => {
+        render(<Post />)
+        fireEvent.change(screen.getByTestId('comentarios'), {
+            target: {
+                value: 'Primeiro comentario de testes'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('Primeiro comentario de testes')).toBeInTheDocument();
+        fireEvent.change(screen.getByTestId('comentarios'), {
+            target: {
+                value: 'Segundo comentário de testes'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('Segundo comentário de testes')).toBeInTheDocument();
+
+
+
+
+    })
 });
